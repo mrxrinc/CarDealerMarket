d84af6a562c43c7bbc362188810a01d056e43ae5
mohammad <mrxmirzaei@gmail.com>
2020-08-14 22:46:00 +0430

    changed App.tsx

--- App.tsx
+++ App.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 import {View, StatusBar} from 'react-native';
 import AppNavigator from './src/utils/navigation';
 import {AppProvider} from 'utils/context';
-export default () => {
-  return (
-    <AppProvider>
-      <View style={{flex: 1}}>
-        <StatusBar
-          translucent
-          barStyle="dark-content"
-          backgroundColor="transparent"
-        />
-        <AppNavigator />
-      </View>
-    </AppProvider>
-  );
-};
+export default () => (
+  <AppProvider>
+    <View style={{flex: 1}}>
+      <StatusBar
+        translucent
+        barStyle="dark-content"
+        backgroundColor="transparent"
+      />
+      <AppNavigator />
+    </View>
+  </AppProvider>
+);
